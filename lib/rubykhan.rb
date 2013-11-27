6ed6a093ee3a79b49977a98954a651a1c561cc53
Abby Howell <abby.g.howell@gmail.com>
2013-11-27 12:33:07 -0500

    changed httparty to a mixin and updated the http get methods to khanacademy::playlist.get

--- lib/rubykhan.rb
+++ lib/rubykhan.rb
@@ -13,6 +13,9 @@ end
 module KhanAcademy
 
 	class Topic
+		include HTTParty
+		format :json
+		
 		attr_reader :icon_src, :domain_slug, :relative_url, :web_url, :ka_url, :translated_standalone_title, :translated_title, :gplus_url, :id, :old_key_name, :hide, :title, :child_data, :children, :twitter_url, :translated_description, :deleted_mod_time, :logo_image_url, :in_knowledge_map, :description, :x_pos, :node_slug, :deleted, :facebook_url, :backup_timestamp, :render_type, :background_image_url, :background_image_caption, :assessment_progress_key, :topic_page_url, :extended_slug, :slug, :tags, :kind, :in_topic_browser, :sha, :standalone_title, :y_pos, :current_revision_key, :content_kind
 		
 		def initialize(options)
@@ -25,24 +28,26 @@ module KhanAcademy
 
 		def self.get_info(topic)
 			uri = "http://www.KhanAcademy.org/api/v1/topic/#{topic}"
-			options = JSON.parse HTTParty.get(uri)
+			options = KhanAcademy::Playlist.get(uri)
 			new(options)
 		end
 
 		def self.all(key = "extended_slug")
 			uri = "http://www.khanacademy.org/api/v1/topictree"
-			options = HTTParty.get(uri).to_hash
+			options = KhanAcademy::Playlist.get(uri).to_hash
 			values(options, key)
 		end
 
 		def self.tree
 			uri = "http://www.khanacademy.org/api/v1/topictree"
-			topictree = JSON.parse HTTParty.get(uri)
+			topictree = KhanAcademy::Playlist.get(uri)
 		end
 
 	end
 
 	class Playlist
+		include HTTParty
+		format :json
 		attr_reader :translated_short_display_name, :assessment_items, :relative_url, :file_name, :author_name, :creation_date, :uses_assessment_items, :all_assessment_item_keys, :ka_url, :v_position, :translated_title, :translated_description_html, :display_name, :tracking_document_url, :tags, :summative, :live, :pretty_display_name, :translated_pretty_display_name, :problem_types, :curated_related_videos, :covers, :h_position, :translated_display_name, :sha1, :name, :prerequisites, :assessment_item_tags, :seconds_per_fast_problem, :short_display_name, :tutorial_only, :current_revision_key, :image_url_256, :content_ki, :translated_youtube_id, :has_questions, :keywords, :duration, :id, :description_html, :title, :progress_key, :edit_slug, :author_names, :deleted_mod_time, :description, :extra_properties, :node_slug, :deleted, :backup_timestamp, :date_added, :download_urls, :translated_youtube_lang, :kind, :url, :ka_user_license, :global_id, :sha, :translated_description, :image_url, :youtube_id, :position, :content_kind, :readable_id
 
 		def initialize(options)
@@ -55,13 +60,13 @@ module KhanAcademy
 
 		def self.get_exercises(topic)
 			uri = "http://www.KhanAcademy.org/api/v1/playlists/#{topic}/exercises"
-			options = JSON.parse HTTParty.get(uri)
+			options = KhanAcademy::Playlist.get(uri)
 			options.collect {|json| new(json)}
 		end
 
 		def self.get_videos(topic)
 			uri = "http://www.KhanAcademy.org/api/v1/playlists/#{topic}/videos"
-			options = JSON.parse HTTParty.get(uri)
+			options = KhanAcademy::Playlist.get(uri)
 			options.collect {|json| new(json)}
 		end
 
